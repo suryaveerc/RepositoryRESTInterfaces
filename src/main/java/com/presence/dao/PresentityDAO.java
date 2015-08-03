f3d65dc52d9ef1fe4bb0b7c1ad4fcdbd921532b7
suryaveerc <chauhan.suryaveer@gmail.com>
2015-08-03 19:27:38 -0400

    This version works for GET/PUT/DELETE/POST presentity.

--- src/main/java/com/presence/dao/PresentityDAO.java
+++ src/main/java/com/presence/dao/PresentityDAO.java
@@ -16,8 +16,38 @@ public class PresentityDAO {
 
     private static final String SQL_INSERT = "insert into presentity (domain,username,event,etag,expires,sender,body,received_time ) values "
             + "(?,?,?,?,?,?,?,?)";
+    private static final String SQL_UPDATE = "update presentity set etag=?,expires=?,received_time=?,sender=?,body=? where domain=? AND username=? AND event=? AND etag=?";
     private static final String SQL_SELECT = "select id,body,extra_hdrs,expires from presentity where domain=? AND username=? AND event=? AND etag=? order by received_time";
     private static final String SQL_DELETE = "delete from presentity where domain=? AND username=? AND event=? AND etag=? order by received_time";
+
+    public int update(Presentity presentity, String etag, String domain, String event, String userName) throws Exception {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        int rowsAffected = 0;
+        try {
+            connection = DAOConnectionFactory.getConnection();
+            preparedStatement = connection.prepareStatement(SQL_UPDATE);
+            preparedStatement.setObject(1, presentity.getEtag());
+            preparedStatement.setObject(2, presentity.getExpires());
+            preparedStatement.setObject(3, presentity.getReceived_time());
+            preparedStatement.setObject(4, presentity.getSender());
+            preparedStatement.setObject(5, presentity.getBody());
+            preparedStatement.setObject(6, domain);
+            preparedStatement.setObject(7, userName);
+            preparedStatement.setObject(8, event);
+            preparedStatement.setObject(9, etag);
+            rowsAffected = preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("Error creating connection");
+            e.printStackTrace();
+        } finally {
+            DAOConnectionFactory.closeConnection(connection, preparedStatement, null);
+        }
+        return rowsAffected;
+    }
 
     public int create(Presentity presentity) throws Exception {
 
@@ -95,7 +125,7 @@ public class PresentityDAO {
             preparedStatement.setObject(3, event);
             preparedStatement.setObject(4, etag);
             rowsAffected = preparedStatement.executeUpdate();
-            System.out.println("Delete Staus: "+rowsAffected);
+            System.out.println("Delete Staus: " + rowsAffected);
         } catch (SQLException e) {
             e.printStackTrace();
         } catch (Exception e) {
--- src/main/java/com/presence/services/rest/PresentityService.java
+++ src/main/java/com/presence/services/rest/PresentityService.java
@@ -10,7 +10,10 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.HEAD;
+import static javax.ws.rs.HttpMethod.HEAD;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -40,6 +43,30 @@ public class PresentityService {
             }
         } catch (Exception e) {
             e.printStackTrace();
+            
+            return Response.status(500).entity("Server was unable to process the request.").build();
+        }
+        return Response.status(201).build();
+    }
+    /* This method allows to insert new presentity into the repository */
+
+    @PUT
+    @Path("/{presentityId}")
+    @Consumes("application/json")
+    public Response updatePresentity(@PathParam("presentityId") String presentityId, Presentity presentity, @QueryParam("event") String event, @QueryParam("etag") String etag) {
+        System.out.println("****************************");
+        System.out.println(presentity);
+        String userName = presentityId.substring(0, presentityId.indexOf('@'));
+        String domain = presentityId.substring(presentityId.indexOf('@') + 1);
+        PresentityDAO presentityDAO = new PresentityDAO();
+        System.out.println(presentity);
+        try {
+            int status = presentityDAO.update(presentity,etag, domain, event, userName);
+            if (status == 0) {
+                return Response.status(204).entity("No record updated.").build();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
             return Response.status(500).entity("Server was unable to process the request.").build();
         }
         return Response.ok().build();
@@ -48,8 +75,8 @@ public class PresentityService {
     @GET
     @Path("/{presentityId}")
     @Produces({MediaType.APPLICATION_JSON})
-    public Response getPresentityByUserAndDomain(@PathParam("presentityId") String presentityId, @QueryParam("event") String event, @QueryParam("etag") String etag, @QueryParam("return") String fetch) {
-        System.out.println("Query for: " + presentityId + " : " + event+ " : " + etag);
+    public Response getPresentityByUserAndDomain(@PathParam("presentityId") String presentityId, @QueryParam("event") String event, @QueryParam("etag") String etag) {
+        System.out.println("Query for: " + presentityId + " : " + event + " : " + etag);
         try {
             String userName = presentityId.substring(0, presentityId.indexOf('@'));
             String domain = presentityId.substring(presentityId.indexOf('@') + 1);
@@ -59,17 +86,14 @@ public class PresentityService {
             presentityList = presentityDAO.find(domain, userName, event, etag);
 
             ResponseBuilder response;
-            if (fetch != null && fetch.equalsIgnoreCase("true")) {
-                entity = new GenericEntity<List<Presentity>>(presentityList) {
-                };
+
+            if (!presentityList.isEmpty()) {
+                entity = new GenericEntity<List<Presentity>>(presentityList) {};
                 response = Response.ok(entity);
             } else {
-                if (!presentityList.isEmpty()) {
-                    response = Response.ok();
-                } else {
-                    response = Response.status(Response.Status.NOT_FOUND).entity("Requested resource could not be found.");
-                }
+                response = Response.status(Response.Status.NOT_FOUND).entity("Requested resource could not be found.");
             }
+
             return response.build();
         } catch (Exception e) {
             e.printStackTrace();
@@ -81,7 +105,7 @@ public class PresentityService {
     @Path("/{presentityId}")
     public Response deletePresentity(@PathParam("presentityId") String presentityId, @QueryParam("event") String event, @QueryParam("etag") String etag) {
         System.out.println("Delete request for: " + presentityId + " : " + event + " : " + etag);
-        
+
         int status = 0;
         try {
             String userName = presentityId.substring(0, presentityId.indexOf('@'));
@@ -103,4 +127,32 @@ public class PresentityService {
     public String hello() {
         return "Hello, Working";
     }
+
+    @HEAD
+    @Path("/{presentityId}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Response checkPresentity(@PathParam("presentityId") String presentityId, @QueryParam("event") String event, @QueryParam("etag") String etag) {
+        System.out.println("Query for: " + presentityId + " : " + event + " : " + etag);
+        try {
+            String userName = presentityId.substring(0, presentityId.indexOf('@'));
+            String domain = presentityId.substring(presentityId.indexOf('@') + 1);
+            PresentityDAO presentityDAO = new PresentityDAO();
+            List<Presentity> presentityList;
+            GenericEntity< List< Presentity>> entity;
+            presentityList = presentityDAO.find(domain, userName, event, etag);
+
+            ResponseBuilder response;
+
+            if (!presentityList.isEmpty()) {
+                response = Response.ok();
+            } else {
+                response = Response.status(Response.Status.NOT_FOUND).entity("Requested resource could not be found.");
+            }
+            return response.build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Response.status(500).entity("Server was unable to process the request.").build();
+        }
+    }
+
 }
